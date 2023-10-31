0b9d5175e5e8feecd8a545c5def1e599c4758cc5
gallifrey <956465331@qq.com>
2023-10-31 09:52:36 +0800

    feat: 添加API Mock层方便测试

--- lib/api/index.dart
+++ lib/api/index.dart
@@ -7,10 +7,17 @@ abstract class ApiProvider {
   SessionApi get session;
 }
 
+// 网络请求的实现 调用后端接口
 class ApiProviderImpl extends ApiProvider {
   final Dio dio;
   ApiProviderImpl(this.dio);
 
   @override
   SessionApi get session => SessionApiImpl(dio);
+}
+
+// Mock的实现 用于测试
+class ApiProviderMock extends ApiProvider {
+  @override
+  SessionApi get session => throw UnimplementedError();
 }
--- lib/global/index.dart
+++ lib/global/index.dart
@@ -1,12 +1,15 @@
 import 'package:dio/dio.dart';
 
 import '../api/index.dart';
+import 'config.dart';
 import 'dio.dart';
 
 class GlobalObjects {
+  //dio 用于网络请求
   static final Dio dio = getDioClient();
+  //apiProvider API接口的调用 用于调用后端接口 也可以用于mock
+  static final ApiProvider apiProvider = GlobalConfig.isMock ? ApiProviderMock() : ApiProviderImpl(dio);
 
   //token
   static String token = 'none';
-  static final ApiProvider apiProvider = ApiProviderImpl(dio);
 }
