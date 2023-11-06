b19e291c78a3394c378424675d66bae35916bb70
gallifrey <956465331@qq.com>
2023-11-07 01:19:53 +0800

    fix: 修复https请求失败问题

--- lib/global/config.dart
+++ lib/global/config.dart
@@ -1,7 +1,7 @@
 class GlobalConfig {
   static const String workBaseUrl = 'https://api.tbghg.top';
   static const String testBaseUrl = 'localhost:8080';
-  static const String qiniuKodoUrl = 'http://up-cn-east-2.qiniup.com';
+  static const String qiniuKodoUrl = 'https://up-cn-east-2.qiniup.com';
   static const String? proxy = null;
   static const bool isUseTestBaseUrl = false;
   static const bool isDebug = true;
